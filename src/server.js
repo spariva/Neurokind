851f1146408184c5ee970bd455bde0c5e86593ec
Maki <makspariva@gmail.com>
2024-01-30 07:34:08 +0100

    Server in progress. Routes.

--- src/server.js
+++ src/server.js
@@ -2,19 +2,31 @@ require('dotenv').config();
 
 const express = require('express');
 const fs = require('fs').promises;
-const cors = require('cors'); // Agregamos la librería 'cors'
+const cors = require('cors'); 
 const app = express();
 const PORT = process.env.PORT;
+const tareasFilePath = 'tasks.json';
+//Paths:
+const path = require('path');
+const basePath = path.resolve(__dirname, '..');
+const publicPath = path.join(basePath, 'public');
+const indexPath = path.join(basePath, 'public/index.html');
 
 app.use(express.json());
-app.use(cors()); // Habilitamos CORS
+app.use(cors()); 
+app.use(express.urlencoded({ extended: true })); // Para que funcione correctamente el formulario
+app.use(express.static(publicPath)); // Para servir archivos estáticos
 
-const tareasFilePath = 'tareas.json';
+// Servir el index.html cuando se solicite la ruta raíz
+app.get('/', function(req, res) {
+  res.sendFile(indexPath);
+});
 
+// 
 app.get('/api/tareas', async (req, res) => {
   try {
     const data = await fs.readFile(tareasFilePath, 'utf-8');
-    const tareas = JSON.parse(data);
+    const tareas = JSON.parse(data); // Redundante, en todo caso comprueba que sea JSON válido, si no lo es, devuelve un error. Y la cosa es que paso de JSON a objeto y luego de objeto a JSON. Podría devolver directamente el JSON. 
     res.json(tareas);
   } catch (error) {
     console.error('Error al leer el archivo de tareas:', error);
