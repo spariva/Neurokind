82bafbeac9b10943bb3ff22bbb1d5b1f557cf11e
Maki <makspariva@gmail.com>
2024-01-31 19:12:11 +0100

    basic version, rerouting.

--- src/server.js
+++ src/server.js
@@ -17,7 +17,81 @@ const PORT = process.env.PORT;
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Servir archivos estáticos desde el directorio actual
-app.use(express.static(__dirname));
+app.use(express.static(publicPath));
+
+// Servir el index.html cuando se solicite la ruta raíz, PERO parece que si lo quito no pasa nada.
+app.get('/', function(req, res) {
+    res.sendFile(indexPath);
+  });
+
+// Obtener todas las tareas. Si quito este no pinta nada en el navegador.
+app.get('/tareas', (req, res) => {
+    fs.readFile('tareas.json', (err, data) => {
+        if (err) throw err;
+        let tareas = [];
+        if (data.toString()) {
+            tareas = JSON.parse(data);
+        }
+        res.send(tareas);
+    });
+});
+
+// Obtener tareas por su color
+app.get('/tareas/:color', (req, res) => {
+    const color = req.params.color;
+
+    fs.readFile('tareas.json', (err, data) => {
+        if (err) throw err;
+
+        let tareas = [];
+        let tareasColor = [];
+        if (data.toString()) {
+            tareas = JSON.parse(data);
+        }
+        for (let i = 0; i < tareas.length; i++) {
+            if (tareas[i].color === color) {
+                tareasColor.push(tareas[i]);
+            }
+        }
+        res.send(tareasColor);
+    });
+});
+
+
+app.post('/anadirTarea', (req, res) => {
+    const tarea = {
+        id: Date.now(), //su id sera unico segun la hora de creacion
+        nombre: req.body.tarea,
+        categoria: req.body.categoria,
+        fecha: req.body.fecha,
+        color: req.body.color
+    };
+
+    fs.readFile('tareas.json', (err, data) => {
+        if (err && err.code === 'ENOENT') {
+            // Si el archivo no existe, crea uno nuevo
+            return fs.writeFile('tareas.json', JSON.stringify([tarea]), error => {
+                if (error) throw error;
+                res.redirect('/index.html?mensaje=¡Tarea%20añadida y fichero JSON creado!');
+            });
+        } else if (err) {
+            throw err;
+        }
+
+        let tareas = [];
+        if (data.toString()) {
+            tareas = JSON.parse(data);
+        }
+        tareas.push(tarea);
+
+        fs.writeFile('tareas.json', JSON.stringify(tareas), error => {
+            if (error) res.redirect('/index.html?error=Error al añadir la tarea');
+
+            res.redirect('/index.html?mensaje=¡Tarea%20añadida!');
+        });
+    });
+});
+
 
 app.put('/editarTarea/:id', (req, res) => {
     const id = req.params.id;
@@ -74,51 +148,6 @@ app.delete('/eliminarTarea/:id', (req, res) => {
     });
 });
 
-app.post('/anadirTarea', (req, res) => {
-    const tarea = {
-        id: Date.now(), //su id sera unico segun la hora de creacion
-        nombre: req.body.tarea,
-        categoria: req.body.categoria,
-        fecha: req.body.fecha,
-        color: req.body.color
-    };
-
-    fs.readFile('tareas.json', (err, data) => {
-        if (err && err.code === 'ENOENT') {
-            // Si el archivo no existe, crea uno nuevo
-            return fs.writeFile('tareas.json', JSON.stringify([tarea]), error => {
-                if (error) throw error;
-                res.redirect('/index.html?mensaje=¡Tarea%20añadida y fichero JSON creado!');
-            });
-        } else if (err) {
-            throw err;
-        }
-
-        let tareas = [];
-        if (data.toString()) {
-            tareas = JSON.parse(data);
-        }
-        tareas.push(tarea);
-
-        fs.writeFile('tareas.json', JSON.stringify(tareas), error => {
-            if (error) res.redirect('/index.html?error=Error al añadir la tarea');
-
-            res.redirect('/index.html?mensaje=¡Tarea%20añadida!');
-        });
-    });
-});
-
-// Nueva ruta para enviar los datos de las tareas al cliente
-app.get('/tareas', (req, res) => {
-    fs.readFile('tareas.json', (err, data) => {
-        if (err) throw err;
-        let tareas = [];
-        if (data.toString()) {
-            tareas = JSON.parse(data);
-        }
-        res.send(tareas);
-    });
-});
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el http://localhost:${PORT}`);
